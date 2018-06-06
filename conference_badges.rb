7e56f2618ba8cc40da7d2225dbc0d04f5d8c8d8d
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-06 21:51:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ end
 
 def printer(attendees)
   attendees.each do |attendee|
-    puts batch_badge_creator(attendee)
-    puts assign_rooms(attendee)
+    puts batch_badge_creator(attendees)
+    puts assign_rooms(attendees)
   end
 end
