d6c75d3bcd7050b6ddd327cde5bfc9f3ad5f2fc6
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-06 20:42:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def batch_badge_creator(attendees)
   counter = 0
   attendees.each do |attendee|
-    batch_badge_creator[counter] = badge_maker(attendee)
+    attendees[counter] = badge_maker(attendee)
     counter += 1
   end
 end
